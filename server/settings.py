72289d1ccbf8cff89565dd75d09130c2af8fade5
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2020-06-17 10:57:40 -0300

    set false

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'b^e*jiy0x)x878+sl3=kerv39oqavi2!9g^^3s9mz6kjtx08ok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
